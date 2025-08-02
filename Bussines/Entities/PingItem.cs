d5ee4a60e94c99ac0c7afdc6b1b8509154957973
Reniato00 <renyralo@hotmail.com>
2025-08-01 22:10:19 -0600

    Send new way to get pings

---
+++ Bussines/Entities/PingItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PingViewerApp.Bussines.Entities
+{
+    public class PingItem
+    {
+        public string Name { get; set; }
+        public string Host { get; set; }
+    }
+
+    public class  PingDatabase
+    {
+        public List<PingItem> Pings { get; set; }
+    }
+}
