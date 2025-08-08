bdf37334cb6f356e846e015decf0031b47dcdbe3
Reniato00 <renyralo@hotmail.com>
2025-08-08 15:21:39 -0600

    Upload first unit test

--- Testing/Bussines/PingMonitorTests.cs
+++ Testing/Bussines/PingMonitorTests.cs
@@ -1,8 +1,12 @@
 ﻿using Moq;
+using PingViewerApp;
+using PingViewerApp.Bussines.Entities;
 using PingViewerApp.Bussines.Services;
+using Xunit;
 
 namespace Testing.Bussines
 {
+    [TestClass]
     public class PingMonitorTests
     {
         private PingMonitor pingMonitor;
@@ -10,6 +14,14 @@ namespace Testing.Bussines
         public PingMonitorTests()
         {
             pingMonitor = new PingMonitor(pingRequester.Object);
+        }
+
+        [TestMethod]
+        public async Task PingAllAsync_Test() 
+        {
+            var actionMock = new Mock<Action<PingResult>>();
+            await pingMonitor.PingAllAsync(actionMock.Object);
+            pingRequester.Verify(pr => pr.PingEachAsync(It.IsAny<List<PingItem>>(), actionMock.Object), Times.Once);
         }
     }
 }
